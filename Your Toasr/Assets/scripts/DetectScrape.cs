42aeb3c32cbf081a103fcbe78fdc2002e5094a36
Brady Moore <bjmoore@ucsc.edu>
2020-02-02 13:15:56 -0800

    full game loop added

---
+++ Your Toasr/Assets/scripts/DetectScrape.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectScrape : MonoBehaviour
+{
+    public float dist = 0f;
+    public Vector3 startPos;
+    private GameObject cam;
+    private bool countedScore = false; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GameObject.Find("Main Camera");
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist = Vector3.Distance(Vector3.zero, transform.position);
+        if (dist > 80f && !countedScore)
+        {
+            countedScore = true;
+            cam.GetComponent<Score>().score++;
+        }
+    }
+}
---
+++ Your Toasr/Assets/scripts/Score.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Score : MonoBehaviour
+{
+    public int score;
+    public int scoreToWin = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Score: " + score);
+        if (score >= scoreToWin)
+        {
+            SceneManager.LoadScene("EndScene");
+        }
+    }
+}
